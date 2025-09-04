ec9df2ea4f71bdb80469bd5922ad9ab71245da58
Edinei Lucas Silva Souza <edineilucas001@gmail.com>
2025-09-03 21:31:35 -0300

    Start commit exercicio lab0.h

---
+++ Lab0.h
@@ -0,0 +1,24 @@
+/*Escreva um programa de computador que leia 2 números naturais da entrada (menores do que 1 milhão) e imprima na saída uma linha i\
+
+gual a "amigos" ou a "nao amigos" (sem acento), respectivamente indicando que os 2 números da entrada são ou não são amigos. Dize\
+
+mos que dois números são amigos se a soma de todos os divisores próprios de um deles é igual ao outro e vice-versa.
+
+
+
+Por exemplo, 220 e 284 são amigos.
+
+
+
+Você deve compilar seu programa sem mensagens de erro com [gcc -pedantic  -Wall]. Você deve rodar todos os casos de testes anexos\
+
+ e confirmar que seu programa roda corretamente.
+
+
+
+Você deve rodar    o script de linha de comando abaixo e nenhuma saída deve ser produzida.    
+
+
+
+gcc -pedantic -Wall programa.c ; for ((i=1;i<21;i=i+1)); do ./a.out < ins/$i.in    > $i.meu; diff $i.meu ins/$i.out; done
+*/
